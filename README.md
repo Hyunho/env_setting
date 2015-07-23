f5cef07da8573c28977bf97015ecb7e9d3668e5b
hyunho <hyunho@telcoware.com>
2015-07-24 00:28:18 +0900

    add init.sh

--- README.md
+++ README.md
@@ -4,9 +4,23 @@ For bash and vim environment yet.
 
 ## Usage
 
-After clone this repository, you should put a follwing linein `~/.barch` file. 
+After clone this repository, you should put a follwing line in `~/.barch` file. 
 
 ```bash
 source ~/env_setting/bashrc
 ```
+
+And you exectue init scripts
+
+```bash 
+~/env_setting/init.sh
+````
+
+## Reference
+
+Following references help me to make environment
+
+- [for git config in init.sh](enviro://github.com/mimul/dev-environment/blob/master/mac-git.md)  
+
+
 
---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git config --global color.ui auto
+
+git config --global alias.lg 'log --graph --date=short --decorate=short --pretty=format:"%C(white)%h%Creset %C(blue)%cn%Creset %C(green)%cd%Creset %C(red reverse)%d%Creset %C(reset)%s"'
+git config --global alias.dlg 'log --stat --decorate=full --pretty=format:"%C(yellow)%h %C(green)%cr %C(blue)%cn%Creset %C(red reverse)%d%Creset %Creset%s %C(cyan)%b"'
+git config --global alias.mlg 'log --merges --pretty=format:"%C(yellow)%h %Cgreen%ci %Cblue%cn%Creset %C(red reverse)%d%Creset %Creset%s %C(cyan)%b"'
+
